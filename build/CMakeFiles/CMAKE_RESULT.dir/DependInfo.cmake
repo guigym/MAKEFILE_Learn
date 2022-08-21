50f82def194bf6307ca66a7b34023038b6e2d54f
guigym <chunbo_wu2013@163.com>
2022-08-21 21:45:14 +0800

    cmake demon 示例1;

---
+++ build/CMakeFiles/CMAKE_RESULT.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/guigym/personal/cmake/cmake_learn/src/main.cpp" "/home/guigym/personal/cmake/cmake_learn/build/CMakeFiles/CMAKE_RESULT.dir/src/main.cpp.o"
+  "/home/guigym/personal/cmake/cmake_learn/src/module1.cpp" "/home/guigym/personal/cmake/cmake_learn/build/CMakeFiles/CMAKE_RESULT.dir/src/module1.cpp.o"
+  "/home/guigym/personal/cmake/cmake_learn/src/module2.cpp" "/home/guigym/personal/cmake/cmake_learn/build/CMakeFiles/CMAKE_RESULT.dir/src/module2.cpp.o"
+  "/home/guigym/personal/cmake/cmake_learn/src/sub_module.cpp" "/home/guigym/personal/cmake/cmake_learn/build/CMakeFiles/CMAKE_RESULT.dir/src/sub_module.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CMAKE_RESULT.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMAKE_RESULT"
+  "CMAKE_RESULT.pdb"
+  "CMakeFiles/CMAKE_RESULT.dir/src/main.cpp.o"
+  "CMakeFiles/CMAKE_RESULT.dir/src/module1.cpp.o"
+  "CMakeFiles/CMAKE_RESULT.dir/src/module2.cpp.o"
+  "CMakeFiles/CMAKE_RESULT.dir/src/sub_module.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMAKE_RESULT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
