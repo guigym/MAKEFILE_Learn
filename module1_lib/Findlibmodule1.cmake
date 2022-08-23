bb6aaa1ac295bd39029af0330c8317d6261c303b
guigym <chunbo_wu2013@163.com>
2022-08-23 13:56:46 +0800

    增加一些注释;

--- module1_lib/Findlibmodule1.cmake
+++ module1_lib/Findlibmodule1.cmake
@@ -1,8 +1,8 @@
 message(STATUS "try to use the outside lib... START")
 
 #在${CMAKE_CURRENT_LIST_DIR} 路径下寻找module1.h 文件，并把找到的路径保存在LIBMODULE1_INCLUDE_DIR
-find_path(LIBMODULE1_INCLUDE_DIR module1.h ${CMAKE_CURRENT_LIST_DIR}) ##注意这里指定的地址是绝对路径
-find_library(LIBMODULE1_LIBRARY NAMES module1 PATHS ${CMAKE_CURRENT_LIST_DIR})  ##注意这里指定的地址是绝对路径
+find_path(LIBMODULE1_INCLUDE_DIR module1.h /usr/include/ ${CMAKE_CURRENT_LIST_DIR}) ##注意这里指定的地址是绝对路径
+find_library(LIBMODULE1_LIBRARY NAMES module1 PATHS /usr/lib/ ${CMAKE_CURRENT_LIST_DIR})  ##注意这里指定的地址是绝对路径
 
 #message(STATUS ${LIBMODULE1_INCLUDE_DIR})
 #message(STATUS ${LIBMODULE1_LIBRARIES})
